e14b9d7e22601de8a8b90cecc9557e65393b5765
mailofdev <mailofdeveloper@gmail.com>
2025-01-19 11:09:05 +0530

    basic project

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { RadioButton } from 'react-native-elements';
+
+const { width: screenWidth } = Dimensions.get('window');
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{ flex: 1 }}>
+      <RadioButton 
+        title="Test" 
+        checked={selectedSource === 'Test'} 
+      />
+      <RadioButton 
+        title="Server" 
+        checked={selectedSource === 'Server'} 
+      />
+
+      <View>
+        <Text>The Country:</Text>
+        <TextInput defaultValue="Pakistan" />
+        <Text>The Average: 77</Text>
+        <View style={{ width: screenWidth * 77/300, height: 10, backgroundColor: 'blue' }}></View>
+      </View>
+
+      <View>
+        <Text>The Country:</Text>
+        <TextInput defaultValue="Australia" />
+        <Text>The Average: 31</Text>
+        <View style={{ width: screenWidth * 31/300, height: 10, backgroundColor: 'blue' }}></View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
